42cc3da01cb1c331b689a9431ba31c3a60f1e2cc
Logesh Aravind <logaravind1998@gmail.com>
2023-09-29 08:41:09 +0530

    folder structure added

--- app.py
+++ app.py
@@ -1,19 +1,17 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
-import time
 import io
 from PIL import Image
 import base64
 import cv2
 import numpy as np
-import imutils
-import mediapipe as mp
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'your_secret_key_here'
 socketio = SocketIO(app, cors_allowed_origins='*')
 
-@app.route('/', methods=['POST', 'GET'])
+@app.route('/')
 def index():
     return render_template('index.html')
 
@@ -27,39 +25,14 @@ def readb64(base64_string):
 
     return cv2.cvtColor(np.array(pimg), cv2.COLOR_RGB2BGR)
 
-# Initialize MediaPipe Face Detection
-mp_face_detection = mp.solutions.face_detection
-mp_drawing = mp.solutions.drawing_utils
-
 
 @socketio.on('image')
 def image(data_image):
-    import cv2
-    
     frame = readb64(data_image)
-
-    # Draw a vertical blue line on the frame
-    line_x = 200  # Adjust the x-coordinate as needed
-    cv2.line(frame, (line_x, 0), (line_x, frame.shape[0]), (255, 0, 0), 2)
-
-    # Perform face detection
-    with mp_face_detection.FaceDetection(min_detection_confidence=0.5) as face_detection:
-        #frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        results = face_detection.process(frame)
-
-        # If faces are detected, draw bounding boxes
-        if results.detections:
-            for detection in results.detections:
-                bboxC = detection.location_data.relative_bounding_box
-                ih, iw, _ = frame.shape
-                x, y, w, h = int(bboxC.xmin * iw), int(bboxC.ymin * ih), int(bboxC.width * iw), int(bboxC.height * ih)
-                cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
-
-    imgencode = cv2.imencode('.jpeg', frame, [cv2.IMWRITE_JPEG_QUALITY, 40])[1]
-    stringData = base64.b64encode(imgencode).decode('utf-8')
-    b64_src = 'data:image/jpeg;base64,'
-    stringData = b64_src + stringData
-    emit('response_back', stringData)
+    # Encode the processed frame and send it back to the client
+    _, buffer = cv2.imencode('.jpg', frame)
+    data_image = base64.b64encode(buffer).decode('utf-8')
+    emit('response_back', data_image)
 
 if __name__ == '__main__':
-    socketio.run(app, port=8000, debug=True)
+    socketio.run(app, debug=True)
